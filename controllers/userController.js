d3538b8569aaedf82cd42d6fe27e16550b980763
Mayank Harsani <mayank.harsani@gmail.com>
2022-07-12 15:26:09 +0530

    profile-image

--- controllers/userController.js
+++ controllers/userController.js
@@ -110,16 +110,18 @@ exports.getKeywords = catchAsync(async (req, res, next) => {
 
   const username = req.query['username']
   const userdetails = await axios.get(
-    `https://api.twitter.com/2/users/by/username/${username}`, {
+    `https://api.twitter.com/2/users/by/username/${username}?user.fields=profile_image_url`, {
     headers: { 
       'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAPhjWAEAAAAA0s%2FWHeOO%2BvP96lKjfw6uZBgWCzI%3DAj6em26R9yL15TuETqDuXYVphaLRGFnTPgWKZtm3puPWjfTx04', 
     }
   }
   );
+  console.log(userdetails.data);
   const userId = userdetails.data['data']['id']
+  const profileImageUrl = userdetails.data['data']['profile_image_url']
 
   const response = await axios.get(
-    `https://api.twitter.com/2/tweets/search/recent?query=from%3A%20${username}&tweet.fields=author_id`, {
+    `https://api.twitter.com/2/tweets/search/recent?query=from%3A%20${username}&tweet.fields=author_id&max_results=100`, {
     headers: { 
       'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAPhjWAEAAAAA0s%2FWHeOO%2BvP96lKjfw6uZBgWCzI%3DAj6em26R9yL15TuETqDuXYVphaLRGFnTPgWKZtm3puPWjfTx04', 
       'Cookie': 'guest_id=v1%3A165754080682838762; guest_id_ads=v1%3A165754080682838762; guest_id_marketing=v1%3A165754080682838762; personalization_id="v1_Sy1q3AX5HK750dL3bNbuVg=="'
@@ -162,7 +164,8 @@ exports.getKeywords = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       'tweetKeywords': keywordResponse.data,
-      'userLists': userListsText
+      'userLists': userListsText,
+      'profileImageUrl': profileImageUrl
     }
   });
 });
